080b157246dbceaa396207e83213c0858a18610a
qwegat <63029208+qwegat@users.noreply.github.com>
2026-09-15 14:00:18 +0900

    PWA対応？

--- vite-project/vite.config.ts
+++ vite-project/vite.config.ts
@@ -1,11 +1,37 @@
 import react, { reactCompilerPreset } from '@vitejs/plugin-react'
 import babel from '@rolldown/plugin-babel'
 import { defineConfig } from 'vite'
+import { VitePWA } from "vite-plugin-pwa";
+
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    babel({ presets: [reactCompilerPreset()] })
+    babel({ presets: [reactCompilerPreset()] }),
+    VitePWA({
+      devOptions: { enabled: true },
+      manifest: {
+        name: "とりあえずメモ",
+        /*icons: [
+          {
+            sizes: "192x192",
+            src: "icon-192x192.png",
+            type: "image/png",
+          },
+          {
+            sizes: "512x512",
+            src: "icon-512x512.png",
+            type: "image/png",
+          },
+          {
+            sizes: "512x512",
+            src: "icon-512x512.png",
+            type: "image/png",
+            purpose: "maskable",
+          },
+        ],*/
+      },
+    }),
   ],
-})
+});
