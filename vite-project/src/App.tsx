d128d77f90fd4c562440119ce49553c34b9da3f7
KokiNoshita <noshiyan.72942@gmail.com>
2026-09-15 15:51:27 +0900

    ダミーのボタンを追加した

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -8,6 +8,8 @@ function App() {
         <h1>とりあえずメモ</h1>
         <img width="50" height="50" src="https://img.icons8.com/ios/50/settings--v1.png" alt="settings--v1" />
       </div>
+
+      <button id="add-memo-button" type="button">メモ追加</button>
     </>
   )
 }
